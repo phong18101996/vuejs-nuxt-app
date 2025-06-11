21d74b04c2b39edabf1038a931a3df2fca86b6fc
phongnguyendev <phong.nguyen@fractal.vn>
2025-06-11 17:45:17 +0700

    update route redirect

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,4 +1,7 @@
 export default defineNuxtRouteMiddleware((to, from) => {
+  if (to.path === '/') {
+    return navigateTo('/login')
+  }
   const currentUser = useCookie('currentUser')
 
   const publicPages = ['/login', '/register']
